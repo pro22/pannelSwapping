8590a1fb8dae057ae0320d87e42941050e5a3735
pro <procheta07n@gmail.comgit config --global user.name prothoma>
2015-04-18 22:53:08 +0600

    panel

---
+++ panelSwapping/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace panelSwapping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Location = new System.Drawing.Point(76, 69);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(147, 88);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.panel2.Location = new System.Drawing.Point(401, 69);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(167, 114);
+            this.panel2.TabIndex = 1;
+            this.panel2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseDown);
+            this.panel2.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseMove);
+            this.panel2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseUp);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkRed;
+            this.panel3.Location = new System.Drawing.Point(76, 221);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(134, 88);
+            this.panel3.TabIndex = 1;
+            this.panel3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseDown);
+            this.panel3.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseMove);
+            this.panel3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel3_MouseUp);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.Orange;
+            this.panel4.Location = new System.Drawing.Point(401, 221);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(167, 89);
+            this.panel4.TabIndex = 1;
+            this.panel4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseDown);
+            this.panel4.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseMove);
+            this.panel4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(681, 370);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ panelSwapping/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace panelSwapping
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool panel1_Selected = false;
+        bool panel2_Selected = false;
+        bool panel3_Selected = false;
+        bool panel4_Selected = false;
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            panel1_Selected = true;
+        }
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            panel2_Selected = true;
+        }
+        private void panel3_MouseDown(object sender, MouseEventArgs e)
+        {
+            panel3_Selected = true;
+        }
+        private void panel4_MouseDown(object sender, MouseEventArgs e)
+        {
+            panel4_Selected = true;
+        }
+
+
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            panel1_Selected = false;
+        }
+        private void panel2_MouseUp(object sender, MouseEventArgs e)
+        {
+            panel2_Selected = false;
+        }
+        private void panel3_MouseUp(object sender, MouseEventArgs e)
+        {
+            panel3_Selected = false;
+        }
+        private void panel4_MouseUp(object sender, MouseEventArgs e)
+        {
+            panel4_Selected = false;
+        }
+
+
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (panel1_Selected)
+            {
+                int x = (Cursor.Position.X - 50);
+                int y = (Cursor.Position.Y - 50);
+                panel1.Location = new Point(x, y);
+            }
+        }        
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (panel2_Selected)
+            {
+                int x = (Cursor.Position.X - 50);
+                int y = (Cursor.Position.Y - 50);
+                panel2.Location = new Point(x, y);
+            }
+        }
+        private void panel3_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (panel3_Selected)
+            {
+                int x = (Cursor.Position.X - 50);
+                int y = (Cursor.Position.Y - 50);
+                panel3.Location = new Point(x, y);
+            }
+        }
+        private void panel4_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (panel4_Selected)
+            {
+                int x = (Cursor.Position.X - 50);
+                int y = (Cursor.Position.Y - 50);
+                panel4.Location = new Point(x, y);
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        
+    }
+}
